2dd2171c5b0c7665eb1f984377cbb5123bdb7874
Justin LaPolla <j.a.lapolla@gmail.com>
2015-09-19 23:17:54 +0000

    Rename ext_vundle function to ext_vundle_install
    - Continuing the last commit where we renamed ext_vundle.mk to
      ext_vundle_install.mk. I split it into separate commits to make sure
      Git recognized that the file was moved first.

--- extensions/ext_vundle_install.mk
+++ extensions/ext_vundle_install.mk
@@ -1,8 +1,8 @@
 # MODULE EXTENSION
-# ext_vundle - create Vundle installation target at prefix
+# ext_vundle_install - create Vundle installation target at prefix
 #
 # USAGE
-# target_name := $(call ext_vundle,[<prefix>])
+# target_name := $(call ext_vundle_install,[<prefix>])
 #
 # DESCRIPTION
 # Creates target with rule to install Vundle at the supplied <prefix>.
@@ -17,27 +17,27 @@
 # The name of the newly created target.
 #
 # VARIABLES
-# $(__ext_vundle_template) [PRIVATE]
+# $(__ext_vundle_install_template) [PRIVATE]
 # Rule template used for creating Vundle installation targets.
-# $(__ext_vundle_template) is a function that is called, and then
-# $(eval)'d to create a new Vundle installation target.
+# $(__ext_vundle_install_template) is a function that is called, and
+# then $(eval)'d to create a new Vundle installation target.
 #
-# $(__ext_vundle_comma) [PRIVATE]
+# $(__ext_vundle_install_comma) [PRIVATE]
 # Contains a comma (,) character.
 
 # Include helpdoc.mk library, if available
 -include helpdoc.mk
 
 # Store "," character
-__ext_vundle_comma := ,
+__ext_vundle_install_comma := ,
 
 # Rule template for new Vundle installation target
-define __ext_vundle_template
+define __ext_vundle_install_template
 $(call helpdoc,$(1).vim/bundle/Vundle.vim/README.md,Install Vundle under "$(1).vim" prefix. Vundile is a Vim plugin manager. README.md is used to detect if Vundle is already installed.)
 $(1).vim/bundle/Vundle.vim/README.md:
 	mkdir -p $(1).vim/bundle
 	git clone --depth 1 'https://github.com/VundleVim/Vundle.vim.git' $(1).vim/bundle/Vundle.vim
 endef
 
-# "ext_vundle" function definition
-ext_vundle = $(eval $(call __ext_vundle_template,$(1)))$(1).vim/bundle/Vundle.vim/README.md
+# "ext_vundle_install" function definition
+ext_vundle_install = $(eval $(call __ext_vundle_install_template,$(1)))$(1).vim/bundle/Vundle.vim/README.md
